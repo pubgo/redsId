5fe1ef43a1a409f4c3560e06e18146df9ba903b4
kooksee <kooksee@163.com>
2019-05-30 19:25:44 +0800

    [更新] init

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# redsID
-get lock id from redis
+# redsId
+
+> 为每一个worker实例获取一个递增的连续的唯一的ID
+
+1. 启动worker实例
+2. worker连接redis, 同时从0开始检查, 本实例想要获取的ID存不存在
+3. 不存在, 设置ID为key, 同时设置过期时间
+4. 定时1s 去检查key是否过期
+5. 实例退出后, 那么redis的key过期, 实例重新启动之后, 那么通过检查重新获取key
---
+++ redname.go
@@ -0,0 +1,116 @@
+package redname
+
+import (
+	"errors"
+	"fmt"
+	"github.com/go-redis/redis"
+	"time"
+)
+
+func New() *Cfg {
+	return &Cfg{
+		NamePrefix:  "redname",
+		ExpiredTime: time.Second * 5,
+		RetryTime:   time.Second * 2,
+		id:          -1,
+		_stop:       make(chan bool),
+		_err:        make(chan error, 1000),
+	}
+}
+
+type Cfg struct {
+	// 名字前缀
+	// 默认: redname
+	NamePrefix string
+
+	// 过期时间
+	// 默认: 5s
+	ExpiredTime time.Duration
+
+	// 重试时间
+	// 默认: 2s
+	RetryTime time.Duration
+
+	client *redis.Client
+
+	// 从redis获取的实例ID
+	// 默认-1
+	id int
+
+	_stop        chan bool
+	_errCallBack func(err error)
+	_err         chan error
+}
+
+func (t *Cfg) SetRedisClient(client *redis.Client) {
+	t.client = client
+}
+
+func (t *Cfg) getClient() *redis.Client {
+	for {
+		if t.client == nil {
+			t._err <- errors.New("redis client is nil")
+			time.Sleep(time.Second)
+		}
+
+		return t.client
+	}
+}
+
+// 检查name 是否存在, 不存在则设置时间
+func (t *Cfg) checkName(name string, id int) bool {
+	for {
+		ok, err := t.getClient().SetNX(name, id, t.ExpiredTime).Result()
+		if err == redis.Nil {
+			err = nil
+		}
+
+		if err != nil {
+			t._err <- err
+			time.Sleep(t.RetryTime)
+			continue
+		}
+
+		return ok
+	}
+}
+
+func (t *Cfg) Start(fn func(err error)) {
+	t._errCallBack = fn
+
+	go func() {
+		for {
+			select {
+			case <-t._stop:
+				return
+			case _err := <-t._err:
+				if t._errCallBack != nil {
+					t._errCallBack(_err)
+				}
+			case <-time.NewTimer(time.Second).C:
+				_id := t.GetID()
+				t.checkName(fmt.Sprintf("%s%d", t.NamePrefix, _id), _id)
+			}
+		}
+	}()
+}
+
+func (t *Cfg) Stop() {
+	t._stop <- true
+}
+
+func (t *Cfg) GetID() int {
+	if t.id > 0 {
+		return t.id
+	}
+
+	for i := 1; ; i++ {
+		_name := fmt.Sprintf("%s%d", t.NamePrefix, i)
+		if t.checkName(_name, i) {
+			t.id = i
+			break
+		}
+	}
+
+	return t.id
+}
